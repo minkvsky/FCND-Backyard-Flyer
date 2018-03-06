2b3994dc45f04926b17f2cb08730578568e7a0f2
minkvsky <weifatianxia@gmail.com>
2018-03-06 19:32:34 +0800

    update backyard_flyer.py

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -41,7 +41,17 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
         """
-        pass
+        def planedistance(p1, p2):
+            dis = ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
+            return dis
+        if self.flight_state == States.TAKEOFF:
+            # self.all_waypoint = self.calculate_box()
+            self.waypoint_transition()
+
+        if self.flight_state == States.WAYPOINT:
+            if planedistance(self.local_position[:2], self.target_position[:2]) < 1:
+                self.landing_transition()
+
 
     def velocity_callback(self):
         """
@@ -49,7 +59,10 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
         """
-        pass
+        if self.flight_state == States.LANDING:
+            if ((self.global_position[2] - self.global_home[2] < 0.1) and abs(self.local_position[2]) < 0.01):
+                self.disarming_transition()
+
 
     def state_callback(self):
         """
@@ -57,14 +70,24 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
         """
-        pass
+        if not self.in_mission:
+            return
+        if self.flight_state == States.MANUAL:
+            self.arming_transition()
+        elif self.flight_state == States.ARMING:
+            self.takeoff_transition()
+        elif self.flight_state == States.DISARMING:
+            self.manual_transition()
 
     def calculate_box(self):
         """TODO: Fill out this method
         
         1. Return waypoints to fly a box
         """
-        pass
+        all_waypoints = [(10, 0), (10, 10), (0, 10), (0, 0)]
+        # self.all_waypoints = all_waypoints
+        print('calculate_box:{}'.format(all_waypoints))
+        return all_waypoints
 
     def arming_transition(self):
         """TODO: Fill out this method
@@ -75,6 +98,13 @@ class BackyardFlyer(Drone):
         4. Transition to the ARMING state
         """
         print("arming transition")
+        self.take_control()
+        self.arm()
+
+        self.set_home_position(self.global_position[0],
+                              self.global_position[1],
+                              self.global_position[2])
+        self.flight_state = States.ARMING
 
     def takeoff_transition(self):
         """TODO: Fill out this method
@@ -85,6 +115,11 @@ class BackyardFlyer(Drone):
         """
         print("takeoff transition")
 
+        target_altitude = 3.0
+        self.target_position[2] = target_altitude
+        self.takeoff(target_altitude)
+        self.flight_state = States.TAKEOFF
+
     def waypoint_transition(self):
         """TODO: Fill out this method
     
@@ -92,6 +127,17 @@ class BackyardFlyer(Drone):
         2. Transition to WAYPOINT state
         """
         print("waypoint transition")
+
+        # todo
+        self.all_waypoints = self.calculate_box()
+        print(self.all_waypoints)
+        for point in self.all_waypoints:
+            print(point)
+            self.cmd_position(point[0],
+                             point[1],
+                             self.target_position[2], 0)
+            time.sleep(3)
+        self.flight_state = States.WAYPOINT
 
     def landing_transition(self):
         """TODO: Fill out this method
@@ -101,6 +147,9 @@ class BackyardFlyer(Drone):
         """
         print("landing transition")
 
+        self.land()
+        self.flight_state = States.LANDING
+
     def disarming_transition(self):
         """TODO: Fill out this method
         
@@ -108,6 +157,9 @@ class BackyardFlyer(Drone):
         2. Transition to the DISARMING state
         """
         print("disarm transition")
+
+        self.disarm()
+        self.flight_state = States.DISARMING
 
     def manual_transition(self):
         """This method is provided
